434b851a54d3846a6d745bd2090799d3db1a4637
Kate Scott <katy.scott@me.com>
2026-09-16 14:54:19 +0100

    Allow Klaviyo script bundles in CSP script-src

--- app/entry.server.jsx
+++ app/entry.server.jsx
@@ -22,6 +22,12 @@ export default async function handleRequest(
     checkoutDomain: context.env.PUBLIC_CHECKOUT_DOMAIN,
     storeDomain: context.env.PUBLIC_STORE_DOMAIN,
   },
+  scriptSrc: [
+    "'self'",
+    'https://cdn.shopify.com',
+    'https://shopify.com',
+    'https://static.klaviyo.com',
+  ],
   connectSrc: [
     "'self'",
     'https://mercer79-proxy.vercel.app',
